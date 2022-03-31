b0697438fc6d9de978b1eb2b7febd4409fdd2a26
Stephanie Clark <sc.aurora92@gmail.com>
2022-03-31 14:59:50 -0400

    adding jQuery support to framework

--- Greetr.js
+++ Greetr.js
@@ -90,6 +90,26 @@
             this.language = lang;
             this.validate();
             return this;
+        },
+
+        HTMLGreeting: function( selector, formal ) {
+            if(!$) {
+                throw 'jQuery not loaded';
+            }
+            if (!selector) {
+                throw 'Missing jQuery selector';
+            }
+
+            let msg;
+            if (formal) {
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting();
+            }
+            
+            $(selector).html(msg);
+
+            return this;
         }
 
     };
